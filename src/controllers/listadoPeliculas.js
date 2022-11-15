8cc5931d8ad4f8dbe41ad95c3491152c50e08852
407 <407@academicos.cesde.edu.co>
2022-11-15 16:50:09 -0500

    se crean posters

--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -21,29 +21,29 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/tadeo3nuevoposter.jpg?alt=media&token=3b31d4c0-3c0b-4317-ba8a-fc47a47e2a84"
     },
     {
-        nombre: "Wakanda forever",
+        nombre: "AMSTERDAMNUEVO",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinnema-33e13.appspot.com/o/imagenes%2FAMSTERDAMNUEVO1.jpg?alt=media&token=05e1293c-bc05-4530-b4b0-117530fb8f9e"
     },
     {
-        nombre: "Wakanda forever",
+        nombre: "Black adam",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinnema-33e13.appspot.com/o/imagenes%2FBlack%20adam%20ultimonuevo.jpg?alt=media&token=cec193af-5c4e-4a12-b3e4-857df7aaf8ba"
     },
     {
-        nombre: "Wakanda forever",
+        nombre: "EL CATACLISMO",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinnema-33e13.appspot.com/o/imagenes%2FEL%20CATACLISMO1.jpg?alt=media&token=9c4edefc-0c8c-4ad6-85e4-9722c7f45d4a"
     },
     {
-        nombre: "Wakanda forever",
+        nombre: "ESTAMOS HECHOS PARA AMARNOS",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinnema-33e13.appspot.com/o/imagenes%2FESTAMOS%20HECHOS%20PARA%20AMARNOS1.jpg?alt=media&token=aff94155-7bc1-4701-9603-94256366dd39"
     },
     {
-        nombre: "Wakanda forever",
+        nombre: "LA MUJER REY NUEVO POSTER",
         duracion:95,
-        poster:"https://firebasestorage.googleapis.com/v0/b/personalsoft-a1bec.appspot.com/o/wakanda.jpg?alt=media&token=5716b762-e877-4512-99ce-4e09cce7f690"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinnema-33e13.appspot.com/o/imagenes%2FLA%20MUJER%20REY%20NUEVO%20POSTER1.jpg?alt=media&token=33f9f767-7206-4dcb-a44a-1b486e963ca1"
     },
     {
         nombre: "Wakanda forever",
@@ -52,15 +52,52 @@ let peliculas=[
     }
 ]
 
-let fila=document.getElementById("fila")
+
+//creamos una referencia al 
+let etiquetafila=document.getElementById("fila")
+
+//si ya tengo datos que normalmente llegan al front como un arreglo de objetos debemos recorrer y depurar dicha informacion
 
 peliculas.forEach(function(pelicula){
-    console.log(pelicula.poster)
+    //para pintar la informacion de cada pelicula debemos aplicar una tecnica conocidad como traversing 
+    //traversin =crear etiquestas html desde js
+    
+    let columna=document.createElement("div")
+    //gerar kia de etiquetas(padres eh hijos)
 
-    let foto=document.createElement("img")
-    foto.src=pelicula.poster
-    foto.classList.add("img-fluid","w-100")
+    columna.classList.add("col")
 
-    fila.appendChild(foto)
+    let tarjeta=document.createElement("div")
+
+    tarjeta.classList.add("card","h-100","shadow")
+
+let poster=document.createElement("img")
+poster.classList.add("img-fluid","w-100")
+poster.src=pelicula.poster
+
+
+let duracion=document.createElement("p")
+
+let nombre=document.createElement("h3")
+nombre.classList.add("text-center","fw-bold")
+nombre.textContent=pelicula.nombre
+
+
+duracion.classList.add("text-start")
+duracion.textContent="Duracion: "+pelicula.duracion+" Min"
+
+//padres eh hijos el orden importa
+etiquetafila.appendChild(columna)
+columna.appendChild(tarjeta)
+
+tarjeta.appendChild(poster)
+
+tarjeta.appendChild(nombre)
+tarjeta.appendChild(duracion)
+
+
+
+
+
 
 })
