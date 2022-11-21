2001c2e48ba870b6ae88503a89badedd34b655a0
lemonpsd <diego.qrz42@gmail.com>
2022-11-21 16:31:53 -0500

    Cine Nobiembre

---
+++ src/controllers/controladorinfo.js
@@ -0,0 +1,30 @@
+//obteniendo datos de la memoria
+
+let peliculasConsultadaEnMemoria=JSON.parse(localStorage.getItem("pelicula"))
+
+console.log(peliculasConsultadaEnMemoria)
+
+
+
+
+//let etiquestaimagenprincipal=document.getElementById("imagenPrincipal")
+
+//asignando valores 
+
+
+let etiquestaimagenPelicula=document.getElementById("imagenPelicula")
+etiquestaimagenPelicula.src=peliculasConsultadaEnMemoria.imagen
+
+let etiquetanombrePelicula=document.getElementById("nombrePelicula")
+
+etiquetanombrePelicula.textContent=peliculasConsultadaEnMemoria.titulo
+
+let etiquetaDuracion=document.getElementById("duracion")
+
+etiquetaDuracion.textContent=peliculasConsultadaEnMemoria.duracion
+
+
+
+
+let etiquestasinopsis=document.getElementById("sipnosisPelicula")
+etiquestasinopsis.textContent=peliculasConsultadaEnMemoria.sinopsis
